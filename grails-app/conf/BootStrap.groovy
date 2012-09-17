d2c8bab4bf78f03da9cd74454e705b6ec10ae2cd
Robert Burns <burnsra@gmail.com>
2012-09-17 10:01:51 -0400

    Log when application starts/ends

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,9 @@
 class BootStrap {
 
     def init = { servletContext ->
+        log.info('This is the init() method of grails-app/conf/BootStrap.groovy.  This should be visible when application starts.')
     }
     def destroy = {
+        log.info('This is the destroy() method of grails-app/conf/BootStrap.groovy.  This should be visible when application ends.')
     }
 }
