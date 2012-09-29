856c0b93ffe478020f8b0965c286c9579adec7d2
Robert Burns <burnsra@gmail.com>
2012-09-29 15:39:05 -0400

    Cleanup forms processing and attributes

--- grails-app/controllers/html5_examples/FormsController.groovy
+++ grails-app/controllers/html5_examples/FormsController.groovy
@@ -6,35 +6,53 @@ class FormsController {
         order:30
         , title:'Forms'
         , subItems: [
-            [action:'color', order:0, title:"color"]
-            ,[action:'date', order:5, title:'date']
-            ,[action:'datetime', order:10, title:'datetime']
-            ,[action:'datetime_local', order:15, title:'datetime-local']
-            ,[action:'email', order:20, title:'email']
-            ,[action:'month', order:25, title:'month']
-            ,[action:'number', order:30, title:'number']
-            ,[action:'range', order:35, title:'range']
-            ,[action:'search', order:40, title:'search']
-            ,[action:'tel', order:45, title:'tel']
-            ,[action:'time', order:50, title:'time']
-            ,[action:'url', order:55, title:'url']
-            ,[action:'week', order:60, title:'week']
+            [action:'color', order:0, title:"Color"]
+            ,[action:'date', order:5, title:'Date']
+            ,[action:'datetime', order:10, title:'Datetime']
+            ,[action:'datetime_local', order:15, title:'Datetime-local']
+            ,[action:'email', order:20, title:'Email']
+            ,[action:'month', order:25, title:'Month']
+            ,[action:'number', order:30, title:'Number']
+            ,[action:'range', order:35, title:'Range']
+            ,[action:'search', order:40, title:'Search']
+            ,[action:'tel', order:45, title:'Tel']
+            ,[action:'time', order:50, title:'Time']
+            ,[action:'url', order:55, title:'URL']
+            ,[action:'week', order:60, title:'Week']
         ]
     ]
 
     def index() { }
 
-    def color() {render(view:"form")}
-    def date() {render(view:"form")}
-    def datetime() {render(view:"form")}
-    def datetime_local() {render(view:"form")}
-    def email() {render(view:"form")}
-    def month() {render(view:"form")}
-    def number() {render(view:"form")}
-    def range() {render(view:"form")}
-    def search() {render(view:"form")}
-    def tel() {render(view:"form")}
-    def time() {render(view:"form")}
-    def url() {render(view:"form")}
-    def week() {render(view:"form")}
+    def color() {}
+    def date() {}
+    def datetime() {}
+    def datetime_local() {}
+    def email() {}
+    def month() {}
+    def number() {
+        def formAvailableAttributes = [
+            max : "Specifies the maximum value allowed"
+            , min : "specifies the minimum value allowed"
+            , step : "specifies the legal number intervals"
+            , value : "Specifies the default value"
+        ]
+        def formAttributes = [ min : "0", max : "10", step : "2" ]
+        [formAvailableAttributes:formAvailableAttributes, formAttributes:formAttributes]
+    }
+    def range() {
+        def formAvailableAttributes = [
+            max : "Specifies the maximum value allowed"
+            , min : "specifies the minimum value allowed"
+            , step : "specifies the legal number intervals"
+            , value : "Specifies the default value"
+        ]
+        def formAttributes = [ min : "0", max : "10" ]
+        [formAvailableAttributes:formAvailableAttributes, formAttributes:formAttributes]
+    }
+    def search() {}
+    def tel() {}
+    def time() {}
+    def url() {}
+    def week() {}
 }
