a1ed4ee182e1b8f3ffed5592da21eb4a666af458
Robert Burns <burnsra@gmail.com>
2012-09-30 09:41:40 -0400

    Show errors for general geolocation use

--- web-app/js/geolocation/general.js
+++ web-app/js/geolocation/general.js
@@ -1,13 +1,31 @@
 var map = $("#map");
 function getLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
     } else {
         map.html("Geolocation is not supported by this browser.");
     }
     map.removeClass("hidden");
 }
+
 function showPosition(position) {
     map.html("Latitude: " + position.coords.latitude + "<br />Longitude: "
             + position.coords.longitude);
+}
+
+function showError(error) {
+    switch (error.code) {
+    case error.PERMISSION_DENIED:
+        map.html("User denied the request for Geolocation.");
+        break;
+    case error.POSITION_UNAVAILABLE:
+        map.html("Location information is unavailable.");
+        break;
+    case error.TIMEOUT:
+        map.html("The request to get user location timed out.");
+        break;
+    case error.UNKNOWN_ERROR:
+        map.html("An unknown error occurred.");
+        break;
+    }
 }
