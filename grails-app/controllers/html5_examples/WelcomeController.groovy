814d45a3ded99387fddb68d4ce05e99a3ca17ec7
Robert Burns <burnsra@gmail.com>
2012-09-30 10:26:55 -0400

    Add subnav for welcome page

--- grails-app/controllers/html5_examples/WelcomeController.groovy
+++ grails-app/controllers/html5_examples/WelcomeController.groovy
@@ -5,6 +5,9 @@ class WelcomeController {
     static navigation = [
         order:1
         ,title:'Home'
+        , subItems: [
+            [group:'Navigation', action:'index', order:0, title:"Home"]
+        ]
     ]
 
     def index() { }
